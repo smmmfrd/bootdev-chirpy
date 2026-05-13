d612ed962f6c12f715c1f7cf2d596697af83addd
Sam Mumford <smmmfrd@gmail.com>
2026-05-13 15:55:09 -0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smmmfrd/bootdev-chirpy
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Run with: go build -o out && ./out
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	fmt.Printf("Starting server on port %v\n", port)
+
+	server.ListenAndServe()
+}
