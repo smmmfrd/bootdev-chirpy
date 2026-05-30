d733ab44fbe93b47ef8d158c4657da71bae7ead8
Sam Mumford <smmmfrd@gmail.com>
2026-05-30 10:25:06 -0700

    Get Chirp Query

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -1,6 +1,9 @@
 -- name: GetAllChirps :many
 SELECT * FROM chirps ORDER BY created_at ASC;
 
+-- name: GetChirp :one
+SELECT * FROM chirps WHERE id = $1;
+
 -- name: CreateChirp :one
 INSERT INTO chirps (id, created_at, updated_at, body, user_id)
 VALUES (
