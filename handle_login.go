0b2591ad137850f5e9e8e987810f79bc5a890a21
Sam Mumford <smmmfrd@gmail.com>
2026-06-04 11:43:29 -0700

    Implement JWT Authentication

--- handle_login.go
+++ handle_login.go
@@ -4,44 +4,66 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/smmmfrd/bootdev-chirpy/internal/auth"
 )
 
 func (cfg *apiConfig) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Password string `json:"password"`
-		Email    string `json:"email"`
+		Password         string `json:"password"`
+		Email            string `json:"email"`
+		ExpiresInSeconds int    `json:"expires_in_seconds"`
 	}
 
 	type response struct {
 		User
+		Token string `json:"token"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
-	c := parameters{}
+	params := parameters{}
 
-	if err := decoder.Decode(&c); err != nil {
+	if err := decoder.Decode(&params); err != nil {
 		respondWithError(w, 500, fmt.Sprintf("Error decoding parameters: %s", err))
 		return
 	}
 
-	user, err := cfg.queries.GetUser(r.Context(), c.Email)
+	user, err := cfg.queries.GetUser(r.Context(), params.Email)
 
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password")
 		return
 	}
 
-	match, err := auth.CheckPasswordHash(c.Password, user.HashedPassword)
+	match, err := auth.CheckPasswordHash(params.Password, user.HashedPassword)
 	if err != nil || !match {
 		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password")
 	}
 
-	respondWithJSON(w, http.StatusOK, User{
-		ID:        user.ID,
-		Email:     user.Email,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
+	expirationTime := time.Hour
+	if params.ExpiresInSeconds > 0 && params.ExpiresInSeconds < 3600 {
+		expirationTime = time.Duration(params.ExpiresInSeconds) * time.Second
+	}
+
+	accessToken, err := auth.MakeJWT(
+		user.ID,
+		cfg.authSecret,
+		expirationTime,
+	)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create auth token")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, response{
+		User: User{
+			ID:        user.ID,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+		},
+		Token: accessToken,
 	})
 }
