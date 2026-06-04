e7117ffdec94a4ec4700d757176946b256871542
Sam Mumford <smmmfrd@gmail.com>
2026-06-04 11:31:22 -0700

    Get Bearer Token Method

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/alexedwards/argon2id"
@@ -75,4 +78,19 @@ func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 	}
 
 	return id, nil
+}
+
+func GetBearerToken(headers http.Header) (string, error) {
+	authHeader := headers.Get("Authorization")
+
+	if authHeader == "" {
+		return "", errors.New("No auth header included in request")
+	}
+
+	splitAuth := strings.Split(authHeader, " ")
+	if len(splitAuth) < 2 || splitAuth[0] != "Bearer" {
+		return "", errors.New("Improper Auth Header")
+	}
+
+	return splitAuth[1], nil
 }
