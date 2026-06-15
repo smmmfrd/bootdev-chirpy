29845e89d95d317e97c7bd5b8c51af2fac462487
Sam Mumford <smmmfrd@gmail.com>
2026-06-15 10:51:05 -0700

    Extract API key from headers method

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -102,3 +102,17 @@ func MakeRefreshToken() string {
 	rand.Read(token)
 	return hex.EncodeToString(token)
 }
+
+func GetAPIKey(headers http.Header) (string, error) {
+	authHeader := headers.Get("Authorization")
+	if authHeader == "" {
+		return "", errors.New("No auth header included in request")
+	}
+
+	splitAuth := strings.Split(authHeader, " ")
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+		return "", errors.New("Improper Auth Header")
+	}
+
+	return splitAuth[1], nil
+}
