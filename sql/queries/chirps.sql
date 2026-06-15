45bac6e9dced790e460b249565ab47e135a51145
Sam Mumford <smmmfrd@gmail.com>
2026-06-15 11:03:06 -0700

    Get Chirps by Author Query

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -1,6 +1,11 @@
 -- name: GetAllChirps :many
 SELECT * FROM chirps ORDER BY created_at ASC;
 
+-- name: GetAllChirpsByAuthor :many
+SELECT * FROM chirps 
+WHERE user_id = $1
+ORDER BY created_at ASC;
+
 -- name: GetChirp :one
 SELECT * FROM chirps WHERE id = $1;
 
